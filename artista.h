c13b393cb3c6a1805717d5e89f593bef86304df2
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-10-27 10:05:15 -0300

    Update artista.h    Projeto 0.5

--- artista.h
+++ artista.h
@@ -1,1 +1,8 @@
+/// Assinatura das funções
 
+void modulo_artista(void);
+char menu_artista(void);
+void tela_cadastrar_artista(void);
+void tela_consultar_artista(void);
+void tela_alterar_artista(void);
+void tela_excluir_artista(void);
