a4609ba7e09ccbf78b666ef190902968e83baad4
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-10-27 10:11:35 -0300

    Update espetaculo.h          Projeto 0.5

--- espetaculo.h
+++ espetaculo.h
@@ -4,3 +4,7 @@ void tela_cadastrar_espetaculo(void);
 void tela_consultar_espetaculo(void);
 void tela_alterar_espetaculo(void);
 void tela_excluir_espetaculo(void);
+void cadastrar_espetaculo(void);
+void consultar_espetaculo(void);
+void alterar_espetaculo(void);
+void excluir_espetaculo(void);
