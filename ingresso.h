b1caff6c9d664122b0c9ec3842d281757115bad1
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-10-27 10:14:47 -0300

    Create ingresso.h

---
+++ ingresso.h
@@ -0,0 +1,6 @@
+void modulo_ingresso(void);
+char menu_ingresso(void);
+void tela_comprar_ingresso(void);
+void tela_reembolsar_ingresso(void);
+void comprar_ingresso(void);
+void reembolsar_ingresso(void);
