bd8585d7d697ecc6d55a2b3d60f60e50eaf0aa5e
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-10-27 10:49:49 -0300

    Update espetaculo.h     Projeto 0.6

--- espetaculo.h
+++ espetaculo.h
@@ -8,3 +8,11 @@ void cadastrar_espetaculo(void);
 void consultar_espetaculo(void);
 void alterar_espetaculo(void);
 void excluir_espetaculo(void);
+
+///
+/// Funções de validação - serão transferidas para o módulo biblioteca.c
+///
+
+int validaId(char []);
+int validaData(char []);
+int validaHorario(char []);
