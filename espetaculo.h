58314833b70b5eaf7ce4d21e86d66e72a3eafc59
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-10-27 10:08:20 -0300

    Create espetaculo.h     Projeto 0.5

---
+++ espetaculo.h
@@ -0,0 +1,6 @@
+void modulo_espetaculo(void);
+char menu_espetaculo(void);
+void tela_cadastrar_espetaculo(void);
+void tela_consultar_espetaculo(void);
+void tela_alterar_espetaculo(void);
+void tela_excluir_espetaculo(void);
