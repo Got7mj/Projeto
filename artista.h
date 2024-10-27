721ebbe1684b9e8b33c7c1821dccc9567e954b69
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-10-27 11:07:53 -0300

    Update artista.h      Projeto 0.6

--- artista.h
+++ artista.h
@@ -6,3 +6,17 @@ void tela_cadastrar_artista(void);
 void tela_consultar_artista(void);
 void tela_alterar_artista(void);
 void tela_excluir_artista(void);
+void cadastrar_artista(void);
+void consultar_artista(void);
+void alterar_artista(void);
+void excluir_artista(void);
+
+///
+/// Funções de validação - serão transferidas para o módulo biblioteca.c
+///
+
+int validaId(char []);
+int validaNome(char []);
+int validaEmail(char []);
+int validaCelular(char []);
+int validaCargo(char []);
