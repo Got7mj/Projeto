654484c9c5e9f8244e22c43999268d822da0cdad
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-11-03 13:19:16 -0300

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -1,1 +1,17 @@
+/// Assinatura das funções
 
+void modulo_usuario(void);
+char menu_usuario(void);
+void tela_cadastrar_usuario(void);
+void tela_consultar_usuario(void);
+void tela_alterar_usuario(void);
+void tela_excluir_usuario(void);
+void cadastrar_usuario(void);
+void consultar_usuario(void);
+void alterar_usuario(void);
+void excluir_usuario(void);
+int validaId(char []);
+int validaNome(char []);
+int validaEmail(char []);
+int validaCelular(char []);
+int validaNasc(char []);
