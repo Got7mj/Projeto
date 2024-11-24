0dd44bfa1406dc09be42f76660e0cbdfd7bcb370
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-11-24 12:29:12 -0300

    Update ingresso.h

--- ingresso.h
+++ ingresso.h
@@ -1,3 +1,12 @@
+typedef struct ingresso Ingresso;
+
+struct ingresso {
+char id[12];
+char preco[3];
+char quantidade[2];
+ int status;
+};
+
 void modulo_ingresso(void);
 char menu_ingresso(void);
 void tela_comprar_ingresso(void);
