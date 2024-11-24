f4aaac74e9c43bbabe025219cb79f3b23e10dccb
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-11-24 12:14:04 -0300

    Update artista.h

--- artista.h
+++ artista.h
@@ -1,12 +1,23 @@
 /// Assinatura das funções
 
-void modulo_artista(void);
-char menu_artista(void);
-void tela_cadastrar_artista(void);
-void tela_consultar_artista(void);
-void tela_alterar_artista(void);
-void tela_excluir_artista(void);
-void cadastrar_artista(void);
-void consultar_artista(void);
-void alterar_artista(void);
-void excluir_artista(void);
+typedef struct artista Artista;
+
+struct artista {
+char id[12];
+char nome[51]; 
+char email[30];
+char celular[12];
+char cargo[30];
+int status;
+};
+
+void modulo_Artista(void);
+char menu_Artista(void);
+void tela_cadastrar_Artista(void);
+void tela_consultar_Artista(void);
+void tela_alterar_Artista(void);
+void tela_excluir_Artista(void);
+void cadastrar_Artista(void);
+void consultar_Artista(void);
+void alterar_Artista(void);
+void excluir_Artista(void);
