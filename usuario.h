3e6ea69335f00b078e99452086b473dba7d8b334
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-11-24 12:24:58 -0300

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -1,5 +1,16 @@
 /// Assinatura das funções
 
+typedef struct usuario Usuario;
+
+struct usuario{
+char id[12];
+char nome[51];   
+char email[30];
+char celular[12];
+char nasc[11];
+int status;
+};
+
 void modulo_usuario(void);
 char menu_usuario(void);
 void tela_cadastrar_usuario(void);
@@ -10,8 +21,3 @@ void cadastrar_usuario(void);
 void consultar_usuario(void);
 void alterar_usuario(void);
 void excluir_usuario(void);
-int validaId(char []);
-int validaNome(char []);
-int validaEmail(char []);
-int validaCelular(char []);
-int validaNasc(char []);
