17d0b06088c91bb84df97eeac209f0b111c4111a
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-11-24 12:35:22 -0300

    Update espetaculo.h

--- espetaculo.h
+++ espetaculo.h
@@ -1,3 +1,12 @@
+typedef struct espetaculo Espetaculo;
+
+struct espetaculo {
+char id[12];
+char data[12];
+char horario[9];
+int status;
+};
+
 void modulo_espetaculo(void);
 char menu_espetaculo(void);
 void tela_cadastrar_espetaculo(void);
@@ -8,11 +17,3 @@ void cadastrar_espetaculo(void);
 void consultar_espetaculo(void);
 void alterar_espetaculo(void);
 void excluir_espetaculo(void);
-
-///
-/// Funções de validação - serão transferidas para o módulo biblioteca.c
-///
-
-int validaId(char []);
-int validaData(char []);
-int validaHorario(char []);
