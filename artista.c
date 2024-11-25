684daf38cbbbb6e57b8b10f3821eba6be1e574fb
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-11-25 15:32:17 -0300

    Update artista.c

--- artista.c
+++ artista.c
@@ -55,12 +55,8 @@ char menu_Artista(void) {
 }
 
 
-void tela_cadastrar_Artista(void) {
-    char id[12];
-    char nome[51];    
-    char email[30];
-    char celular[12];
-    char cargo[30];
+Artista* tela_Preencher_Artista(void) {
+    Artista *art;
     limpaTela();
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -69,36 +65,45 @@ void tela_cadastrar_Artista(void) {
     printf("///            = = = = = = = = Cadastrar Artista = = = = = = =              ///\n");
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
     printf("///                                                                         ///\n");
+	art = (Artista*) malloc(sizeof(Artista));
 do {
     printf("///            ID (apenas números):    ");
-    scanf("%[^\n]", id);
+    scanf("%[^\n]", art->id);
     getchar();
-} while (!validarID(id));    
+} while (!validarID(art->id)); 
+do{	
     printf("///            Nome completo:          ");
-    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+    scanf("%[^\n]", art->nome);
     getchar();
+} while(!validarNome(art->nome));
+do{
     printf("///            E-mail:                 ");
-    scanf("%[A-Za-z0-9@._]", email);
+    scanf(""%[^\n]", art->email);
     getchar();
+} while(!validarEmail("%[^\n]", art->email));
 do {
-	    printf("///           Celular  (apenas números com DDD): ");
-	    scanf("%[^\n]", celular);
-	    getchar();
-} while (!validarCelular(celular));
+	printf("///           Celular  (apenas números com DDD): ");
+	scanf("%[^\n]", art->celular);
+	getchar();
+} while (!validarCelular(art->celular));
+do{
     printf("///            Cargo:                  ");
-    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", cargo);
+    scanf("%[^\n]", art->cargo);
     getchar();
+} while(!validarCargo(art->cargo));
     printf("///                                                                         ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");    
     delay(1);
+    return art;
 }
 
 
-void tela_consultar_Artista(void) {
-    char id[12];
-     limpaTela();
+char* tela_consultar_Artista(void) {
+    char* id;
+    id = (char*) malloc(12*sizeof(char));
+    limpaTela();
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -114,11 +119,13 @@ void tela_consultar_Artista(void) {
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");    
     delay(1);
+    return id;
 }
 
 
-void tela_alterar_Artista(void) {
-    char id[12];
+char* tela_alterar_Artista(void) {
+    char* id;
+    id = (char*) malloc(12*sizeof(char));
      limpaTela();
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -135,11 +142,13 @@ void tela_alterar_Artista(void) {
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     delay(1);
+    return id;
 }
 
 
-void tela_excluir_Artista(void) {
-     char id[12];
+char* tela_excluir_Artista(void) {
+     char* id;
+    id = (char*) malloc(12*sizeof(char));
      limpaTela();
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -156,26 +165,149 @@ void tela_excluir_Artista(void) {
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     delay(1);
+    return id;
+}
+
+
+
+void tela_Erro_Arquivo(void) {
+    limpaTela();
+    printf("\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                      ///\n");
+    printf("///           = = = = = = Erro: Não foi possível acessar = = = = = =     ///\n");
+    printf("/// 	  = = = = = = = = = o banco de dados = = = = = = = = = =     ///\n");
+    printf("///           = = = O arquivo de usuários não foi encontrada = = = =     ///\n");
+    printf("///           = = = = = = = Se o problema persistir, = = = = = = = =     ///\n");
+    printf("/// 	  = = = = entre em contato com o administrador.= = = = =     ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = = =     ///\n");
+    printf("///                                                                      ///\n");
+    printf("///                                                                      ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n\nTecle ENTER para continuar!\n\n");
+    getchar();
+    exit(1);
+
+
+
+void gravar_Artista(Artista *art) {
+	FILE* fp;
+	fp = fopen("artistas.dat", "ab");
+	if (fp == NULL) {
+		tela_Erro_Arquivo();
+	}
+	fwrite(usr, sizeof(Artista), 1, fp);
+	fclose(fp);
+}
+
+
+Artista* buscar_Artista(char* id) {
+	FILE* fp;
+	Artista* art;
+
+	art = (Artista*) malloc(sizeof(Artista));
+	fp = fopen("artistas.dat", "rb");
+	if (fp == NULL) {
+		tela_Erro_Arquivo();
+	}
+	while(fread(usr, sizeof(Artista), 1, fp)) {
+		if ((strcmp(art->id, id) == 0) && (art->status == True)) {
+			fclose(fp);
+			return art;
+		}
+	}
+	fclose(fp);
+	return NULL;
+}
+
+
+void exibir_Artista(Artista* atr) {
+
+	if (usr == NULL) {
+		printf("\n= = = Artista Inexistente = = =\n");
+	} else {
+		printf("\n= = = Artista Cadastrado = = =\n");
+		printf("Id: %s\n", atr->id);
+		printf("Nome: %s\n", atr->nome);
+		printf("Email: %s\n", atr->email);
+		printf("Celular: %s\n", atr->celular);
+		printf("Cargo: %s\n", atr->cargo);		
+		printf("Status: %d\n", atr->status);
+	}
+	printf("\n\nTecle ENTER para continuar!\n\n");
+	getchar();
+}
+
+
+void regravar_Artista(Artista* art) {
+	int achou;
+	FILE* fp;
+	Usuario* atr_Lido;
+
+	atr_Lido = (Artista*) malloc(sizeof(Artista));
+	fp = fopen("artistas.dat", "r+b");
+	if (fp == NULL) {
+		tela_Erro_Arquivo();
+	}
+	// while(!feof(fp)) {
+	achou = False;
+	while(fread(atr_Lido, sizeof(Artista), 1, fp) && !achou) {
+		//fread(atr_Lido, sizeof(Artista), 1, fp);
+		if (strcmp(atr_Lido->id, atr->id) == 0) {
+			achou = True;
+			fseek(fp, -1*sizeof(Artista), SEEK_CUR);
+        	fwrite(atr, sizeof(Artista), 1, fp);
+			//break;
+		}
+	}
+	fclose(fp);
+	free(atr_Lido);
 }
 
 
 void cadastrar_Artista(void) {
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-	tela_cadastrar_Artista();
+	Artista *art;
+	art = tela_Preencher_Artista();
+	gravar_Artista(art);
+	free(art);
 }
 void consultar_Artista(void) {
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-	tela_consultar_Artista();
+	Artista *art;
+	char* id;
+	usr = buscar_Artista(id);
+	exibir_Artista(art);
+	free(art); 
+	free(id);
 }
 void alterar_Artista(void) {
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-	tela_alterar_Artista();
+	Artista *art;
+	char* id;
+	art = buscar_Artista(id);
+	if (usr == NULL) {
+		printf("\n\nArtista não encontrado!\n\n");
+	} else {
+		usr = tela_Preencher_Artista();
+		strcpy(art->id, id);
+		regravar_Artista(art);
+		// Outra opção:
+		// excluir_Artista(id);
+		// gravar_Artista(art);
+		free(art);
+	}
+	free(id);
 }
 void excluir_Artista(void) {
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-	tela_excluir_Artista();
+	Artista *art;
+	char* id;
+	id = tela_Excluir_Artista(id);
+	art = (Artista*) malloc(sizeof(Artista));
+	art = buscar_Artista(id);
+	if (art == NULL) {
+		printf("\n\nArtista não encontrado!\n\n");
+	} else {
+		art->status = False;
+		regravar_Artista(art);
+		free(art);
+	}
+	free(id);
 }
