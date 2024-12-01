6f5c4235602f6e857cabdea270cf26e5d9db97c5
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-12-01 14:23:55 -0300

    Update ingresso.h

--- ingresso.h
+++ ingresso.h
@@ -1,26 +1,24 @@
-#ifndef INGRESSO_H
-#define INGRESSO_H
+/// Assinatura das funções
 
-#include <stdbool.h>  // Para usar o tipo 'bool' que facilita a leitura do código
+typedef struct ingresso Ingresso;
 
-// Definição da estrutura do ingresso
-typedef struct ingresso {
-    char id[12];        // ID do ingresso (apenas números)
-    char preco[12];     // Preço do ingresso (apenas números, usando 12 caracteres para permitir valores maiores)
-    int quantidade;     // Quantidade disponível
-    bool status;        // Status de ativo ou cancelado (true ou false)
-} Ingresso;
+struct ingresso {
+    char id[12];        
+    char preco[12];     
+    int quantidade;     
+int status;
+};
 
-// Funções de manipulação de ingressos
-
+void modulo_Ingresso(void);
+char menu_Ingresso(void);
 Ingresso* tela_Preencher_Ingresso(void);
 char* tela_reembolsar_Ingresso(void);
 void tela_Erro_Arquivo(void);
-void gravar_Ingresso(Ingresso* igs);
-Ingresso* buscar_Ingresso(char* id);
-void exibir_Ingresso(Ingresso* igs);
-void regravar_Ingresso(Ingresso* igs);
+void gravar_Ingresso(Ingresso*);
+Ingresso* buscar_Ingresso(char*);
+void exibir_Ingresso(Ingresso*);
+void regravar_Ingresso(Ingresso*);
 void comprar_Ingresso(void);
 void reembolsar_Ingresso(void);
 
-#endif // INGRESSO_H
+
