d626d4fe5931ce0894ea6b45464c43ca472cc296
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-12-09 20:50:46 -0300

    Update Circo.c

--- Circo.c
+++ Circo.c
@@ -50,7 +50,7 @@ int main(void) {
 /// Funções
 
 void tela_sobre(void) {
-    system("clear||cls");
+    system("clear");
     printf("\n");    
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -68,7 +68,7 @@ void tela_sobre(void) {
 
 
 void tela_equipe(void) {
-    system("clear||cls");
+    system("clear");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -88,7 +88,7 @@ void tela_equipe(void) {
 
 char menu_principal(void) {
     char op;
-    system("clear||cls");
+    system("clear");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
