f620d685a565cc5858a9de7cd9b0b77b6f18b4dd
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-12-22 19:12:55 -0300

    Update ingresso.h

--- ingresso.h
+++ ingresso.h
@@ -1,22 +1,19 @@
-#define ATIVO "ativo"
-#define REEMBOLSADO "reembolsado"
-
 typedef struct ingresso Ingresso;
 
 struct ingresso {
-    char id[12];   
-    int *quantidade;        
-    float preco;  
-char status[];
+    char id[12];    
+    float preco;
+    int quantidade;  // Quantidade como int
+    char status[10]; // Status como string
 };
 
 void modulo_Ingresso(void);
 char menu_Ingresso(void);
-Ingresso* tela_comprar_Ingresso(void);
+Ingresso* tela_comprar_Ingresso(void); 
 char* tela_reembolsar_Ingresso(void);
 void tela_Erro_Arquivo_Ingresso(void);
-void gravar_Ingresso(Ingresso *igs);
+void gravar_Ingresso(Ingresso*);
 Ingresso* buscar_Ingresso(char*);
-void exibir_Ingresso(Ingresso *igs); 
+void exibir_Ingresso(Ingresso*); 
 void comprar_Ingresso(void);
 void reembolsar_Ingresso(void);
