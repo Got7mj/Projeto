894fe77aa7b1991eddd00c83e34c8156ad3b231d
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-12-22 18:04:39 -0300

    Update relatorio.h            Projeto 0.14

--- relatorio.h
+++ relatorio.h
@@ -5,3 +5,8 @@ void listar_Artistas(void);
 void listar_Espetaculos(void);
 void listar_Ingressos(void);
 void listar_Usuarios(void);
+
+void listar_Artistas_Com_Filtro(void);
+void listar_Espetaculos_Com_Filtro(void);
+void listar_Ingressos_Com_Filtro(void);
+void listar_Usuarios_Com_Filtro(void);
