281d8957f6bc7693ba00d20e528ad1b69d046a9f
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-12-22 17:28:29 -0300

    Update relatorio.c        Projeto 0.13

--- relatorio.c
+++ relatorio.c
@@ -10,48 +10,49 @@
 
 
 void modulo_Relatorio(void) {
-	char opcao;
-	do {
-        opcao = menu_Relatorio();
+    char opcao;
+    do {
+        opcao = menu_Relatorio(); 
         switch(opcao) {
-            case '1':  listrar_Artista();
-                    break;
-            case '2':  listrar_Espetaculo();
-                    break;
-            case '3':  listrar_Ingresso();
-                    break;
-            case '4':  listrar_Usuario();
-                    break;
+            case '1':  listar_Artistas();
+		    break;
+            case '2':  listar_Espetaculos(); 
+		    break;
+            case '3':  listar_Ingressos(); 
+		    break;
+            case '4':  listar_Usuarios(); 
+                break;
         } 
     } while (opcao != '0');
 }
 
-char menuRelatorio(void) {
-	char op;
-    limpaTela();
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = = = = =  Menu Relatório = = = = = = = =             ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           1. Listar Artistas                                          ///\n");
-	printf("///           2. Listar Espetaculos                                       ///\n");
-	printf("///           3. Listar Ingressos                                         ///\n");
-	printf("///           4. Listar Usuarios                                          ///\n");
-	printf("///           0. Voltar ao menu anterior                                  ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           Escolha a opção desejada: ");
-	scanf("%c", &op);
-	getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	delay(1);
-	return op;
+
+char menu_Relatorio(void) {
+    char op;
+    limpaTela();  
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = = =  Menu Relatório = = = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           1. Listar Artistas                                          ///\n");
+    printf("///           2. Listar Espetáculos                                       ///\n");
+    printf("///           3. Listar Ingressos                                         ///\n");
+    printf("///           4. Listar Usuarios                                          ///\n");
+    printf("///           0. Voltar ao menu anterior                                  ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Escolha a opção desejada: ");
+    scanf(" %c", &op); 
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    delay(1); 
+    return op;
 }
+
 
 void listar_Artistas(void) {
     FILE *fp;
@@ -63,22 +64,20 @@ void listar_Artistas(void) {
     }
     art = (Artista*)malloc(sizeof(Artista)); 
     while(fread(art, sizeof(Artista), 1, fp)) {
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                        Lista de Artistas                              ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("ID: %d\n", artista.id);
-    printf("Nome: %s\n", artista.nome);
-    printf("Email: %s\n", artista.email);
-    printf("Celular: %s\n", artista.celular);
-    printf("Cargo: %s\n", artista.cargo);
-    printf("-------------------------------------------------\n");
-    printf("Pressione qualquer tecla para sair...");
-    getchar();
+        printf("////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                        Lista de Artistas                              ///\n");
+        printf("/////////////////////////////////////////////////////////////////////////////\n");
+        printf("ID: %d\n", art->id);  
+        printf("Nome: %s\n", art->nome); 
+        printf("Email: %s\n", art->email);  
+        printf("Celular: %s\n", art->celular);  
+        printf("Cargo: %s\n", art->cargo);  
+        printf("-------------------------------------------------\n");
     }
     fclose(fp);
     free(art);
-    return 0;
 }
+
 
 void listar_Espetaculos(void) {
     FILE *fp;
@@ -88,22 +87,20 @@ void listar_Espetaculos(void) {
         printf("Erro ao abrir arquivo de espetáculos.\n");
         return;
     }
-    est = (Espetaculo*) malloc(sizeof(Espetaculo));
+    est = (Espetaculo*)malloc(sizeof(Espetaculo));
     while(fread(est, sizeof(Espetaculo), 1, fp)) {
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                        Lista de Espetáculos                            ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("ID: %d\n",espetaculo.id); 
-    printf("Data: %s\n", espetaculo.data);
-    printf("Horario: %s\n", espetaculo.horario);
-    printf("-------------------------------------------------\n");
-    printf("Pressione qualquer tecla para sair...");
-    getchar();
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                        Lista de Espetáculos                            ///\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("ID: %d\n", est->id);  
+        printf("Data: %s\n", est->data);  
+        printf("Horario: %s\n", est->horario);  
+        printf("-------------------------------------------------\n");
     }
     fclose(fp);
     free(est);
-    return 0;
 }
+
 
 void listar_Ingressos(void) {
     FILE *fp;
@@ -113,43 +110,41 @@ void listar_Ingressos(void) {
         printf("Erro ao abrir arquivo de ingressos.\n");
         return;
     }
-    igs->(Ingresso*) malloc(sizeof(Ingresso));
-    while (fread(&igs, sizeof(Ingresso), 1, fp)) {
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                        Lista de Ingressos                             ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("id: %s\n", igs.id);
-    printf("Quantidade: %d\n", *igs.quantidade);
-    printf("Preço: %.2f\n", igs.preco);
-    printf("-------------------------------------------------\n");
-    printf("Pressione qualquer tecla para sair...");
-    getchar();
+    igs = (Ingresso*)malloc(sizeof(Ingresso));  
+    while(fread(igs, sizeof(Ingresso), 1, fp)) {
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                        Lista de Ingressos                             ///\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("ID: %d\n", igs->id);  
+        printf("Quantidade: %d\n", igs->quantidade);  
+        printf("Preço: %.2f\n", igs->preco);  
+        printf("-------------------------------------------------\n");
     }
     fclose(fp);
     free(igs);
-    return 0;
 }
+
 
 void listar_Usuarios(void) {
     FILE *fp;
-    Usuario *usr;    
+    Usuario *usr;
     fp = fopen("usuarios.dat", "rb");
     if (fp == NULL) {
         printf("Erro ao abrir arquivo de usuários.\n");
         return;
     }
-    usr = (Usuario*) malloc(sizeof(Usuario));
+    usr = (Usuario*)malloc(sizeof(Usuario));
     while(fread(usr, sizeof(Usuario), 1, fp)) {
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                        Lista de Usuários                               ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("id: %s\n", usuario.id);
-    printf("Nome: %s\n", usuario.nome);
-    printf("E-mail: %s\n", usuario.email);
-    printf("Celular: %s\n", usuario.celular);
-    printf("Nasc: %d\n", usuario.idade);
-    printf("-------------------------------------------------\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                        Lista de Usuários                               ///\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("ID: %d\n", usr->id);  
+        printf("Nome: %s\n", usr->nome);  
+        printf("E-mail: %s\n", usr->email);  
+        printf("Celular: %s\n", usr->celular);  
+        printf("Nasc: %d\n", usr->idade);  
+        printf("-------------------------------------------------\n");
     }
-
     fclose(fp);
+    free(usr);
 }
