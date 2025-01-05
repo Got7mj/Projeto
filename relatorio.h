115326ec787368c413784172a79c943b6a3f10b8
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-01-05 15:58:54 -0300

    Update relatorio.h

--- relatorio.h
+++ relatorio.h
@@ -10,3 +10,19 @@ void listar_Artistas_Com_Filtro(void);
 void listar_Espetaculos_Com_Filtro(void);
 void listar_Ingressos_Com_Filtro(void);
 void listar_Usuarios_Com_Filtro(void);
+
+void Ingressos_por_Espetaculo(void);
+void Artistas_por_Espetaculo(void);
+void Ingressos_por_Usuario(void);
+
+char* tela_Ingressos_por_Espetaculo(void);
+char* tela_Artistas_por_Espetaculo(void);
+char* tela_Ingressos_por_Usuario(void);
+
+
+void relat_Ingressos_por_Espetaculo(char*);
+void relat_Artistas_por_Espetaculo(char*);
+void relat_Ingressos_por_Usuario(char*);
+
+void listaTurmasPorHorario(char*);
+char* getNomeUsuario(char*);
