f40797248c83d6f6d017945bae3fcd14e96b2b96
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-01-26 20:26:01 -0300

    Update relatorio.c        Projeto 0.13 e 0.14

--- relatorio.c
+++ relatorio.c
@@ -30,56 +30,10 @@ void modulo_Relatorio(void) {
 		    break;
             case '8':  listar_Usuarios_Com_Filtro(); 
                 break;
-	    case '9':  Ingressos_por_Espetaculo();
-		    break;
-            case '10':  Artistas_por_Espetaculo(); 
-		    break;
-            case '11':  Ingressos_por_Usuario(); 
-		   break;
-	} 
+	    } 
     } while (opcao != '0');
 }
 
-
-void Ingressos_por_Espetaculo(void) {
-
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-
-    char* idEspetaculo;
-
-	idEspetaculo = tela_Ingressos_por_Espetaculo();
-    relat_Ingressos_por_Espetaculo(idEspetaculo);
-    free(idEspetaculo);
-}
-
-
-
-
-void listar_Artistas_por_Espetaculo(void) {
-
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-
-    char* idEspetaculo;
-
-	idEspetaculo = tela_Ingressos_por_Espetaculo();
-    relat_Ingressos_por_Espetaculo(idEspetaculo);
-    free(idEspetaculo);
-}
-
-
-void Ingressos_por_Usuario(void) {
-    
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-
-    char* idUsuario;
-
-	idUsuario = tela_Ingressos_por_Usuario();
-    relat_Ingressos_por_Usuario(idUsuario);
-    free(idUsuario);
-}
 
 
 char menu_Relatorio(void) {
@@ -100,9 +54,6 @@ char menu_Relatorio(void) {
     printf("///           6. Listar Espetáculos com Filtro                            ///\n");
     printf("///           7. Listar Ingressos com Filtro                              ///\n");
     printf("///           8. Listar Usuarios com Filtro                               ///\n");
-    printf("///           9. Listar Ingressos por Espetaculo                          ///\n");
-    printf("///           10. Listar Artistas por Espetaculo                          ///\n");
-    printf("///           11. Listar Ingressos por Usuario                            ///\n");	
     printf("///           0. Voltar ao menu anterior                                  ///\n");
     printf("///                                                                       ///\n");
     printf("///           Escolha a opção desejada: ");
@@ -155,6 +106,7 @@ void listar_Espetaculos(void) {
         printf("///                        Lista de Espetáculos                            ///\n");
         printf("///////////////////////////////////////////////////////////////////////////////\n");
         printf("ID: %s\n", est->id);  
+	printf("Nome: %s\n", est->nome);
         printf("Data: %s\n", est->data);  
         printf("Horario: %s\n", est->horario);  
         printf("-------------------------------------------------\n");
@@ -241,7 +193,7 @@ void listar_Artistas_Com_Filtro(void) {
 
 
 void listar_Espetaculos_Com_Filtro(void) {
-    char id_filtro[50];
+    char id_filtro[50];	z
     FILE *fp;
     Espetaculo *est;
     fp = fopen("espetaculos.dat", "rb");
@@ -253,6 +205,7 @@ void listar_Espetaculos_Com_Filtro(void) {
 	while (fread(est, sizeof(Espetaculo), 1, fp)) {
                 if (strcmp(est->data, data_filtro) == 0) {
                     printf("ID: %s\n", est->id);
+		    printf("Nome: %s\n", est->nome);
                     printf("Data: %s\n", est->data);
                     printf("Horario: %s\n", est->horario);
                     printf("-------------------------------------------------\n");
@@ -313,170 +266,4 @@ void listar_Usuarios_Com_Filtro(void) {
 	}	
 	fclose(fp);
 	free(usr);
-}
-
-
-char* tela_Ingressos_por_Espetaculo (void) {
-    char* idEspetaculo;
-    idEspetaculo = (char*) malloc(4*sizeof(char));
-    limpaTela();
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = =  Ingressos por Espetaculo   = = = = =             ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           Informe o id do Espetaculo: ");
-	scanf("%[0-9]", idEspetaculo);
-	getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	delay(1);
-	return idEspetaculo;
-}
-
-
-char* tela_Artista_por_Espetaculo(void) {
-	char *idEspetaculo;
-	idEspetaculo = (char*) malloc(12*sizeof(char));
-	limpaTela();
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = =  Artistas por Espetaculo  = = = = = =             ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           Informe o id do Espetaculo: ");
-	scanf("%[0-9]", idEspetaculo);
-	getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	delay(1);
-	return idEspetaculo;
-}
-
-
-char* tela_Ingressos_por_Usuario(void) {
-	char* idUsuario;
-	idUsuario = (char*) malloc(7*sizeof(char));
-	limpaTela();
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = = =  Ingressos por Usuario  = = = = = =             ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           Informe o id do Usuario: ");
-	scanf("%[0-9]", idUsuario);
-	getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	delay(1);
-	return idUsuario;
-}
-
-
-
-void relat_Ingressos_por_Espetaculo(char* idEspetaculo) {
-    limpaTela();
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = = = = ID do Espetaculo: %-3s  = = = = =             ///\n", idEspetaculo);
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-    printf("///           ===============================================             ///\n");
-	printf("///           ||  id  ||        Nome do Espetaculo       ||             ///\n");
-    printf("///           ===============================================             ///\n");
-    printf("///           || 12345678901 || Astrogildo de Araújo Alves ||             ///\n");
-    printf("///           || 23456789012 || Benoclécio Barbosa Bastos  ||             ///\n");
-    printf("///           || 34567890123 || Clecionilda Cardoso Castro ||             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-    printf("///           Tecle <ENTER> para continuar...                             ///");
-    getchar();
-}
-
-
-void relat_Artista_por_Espetaculo(char* idEspetaculo) {
-    
-    limpaTela();
-	printf("\n");
-	
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = =  Espetaculo: %12s \n", idEspetaculo);
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-    printf("///           ===============================================             ///\n");
-	printf("///           ||  Artista  ||       Espetaculo       ||             ///\n");
-    printf("///           ===============================================             ///\n");
-    printf("///           ||     T09     || Francês Instrumental I     ||             ///\n");
-    printf("///           ||     T16     || Inglês para Negócios  II   ||             ///\n");
-    printf("///           ||     T42     || Inglês para Negócios III   ||             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-    printf("///           Tecle <ENTER> para continuar...                             ///");
-    getchar();
-    
-}
-
-
-void relat_Ingressos_por_Usuario(char* idUsuario) {
-	char* idUsuario;
-	nomeUsuario = getNomeUsuario(idUsuario);
-    limpaTela();
-	printf("\n");
-	
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///          = = = = =  Usuario: %12s \n", idEspetaculo);           
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-    printf("///           ===============================================             ///\n");
-	printf("///           ||  ID Ingresso  ||        Nome do Usuario       ||             ///\n");
-    printf("///           ===============================================             ///\n");
-    
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-    printf("///           Tecle <ENTER> para continuar...                             ///");
-    getchar();
-	free(nomeUsuario);
-}
-
-
-
-
-char* getNomeUsuario(char* idUsuario) {
-	char *nomeUsuario;
-	Usuario* usr;
-	
-	nomeUsuario = (char*) malloc(51*sizeof(char));
-	usr = (Usuario*) malloc(sizeof(Usuario));
-	usr = buscarProfessor(idUsuario);
-    if (usr == NULL) {
-    	strcpy(nomeUsuario, "");
-    } else {
-    	strcpy(nomeUsuario, usr->nome);
-    }
-    
-	return nomeUsuario;
 }
