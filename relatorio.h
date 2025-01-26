a0e274d2fd4ffe38490d7ada129ce62059f0c437
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-01-26 20:26:55 -0300

    Update relatorio.h            Projeto 0.13 e 0.14

--- relatorio.h
+++ relatorio.h
@@ -10,19 +10,3 @@ void listar_Artistas_Com_Filtro(void);
 void listar_Espetaculos_Com_Filtro(void);
 void listar_Ingressos_Com_Filtro(void);
 void listar_Usuarios_Com_Filtro(void);
-
-void Ingressos_por_Espetaculo(void);
-void Artistas_por_Espetaculo(void);
-void Ingressos_por_Usuario(void);
-
-char* tela_Ingressos_por_Espetaculo(void);
-char* tela_Artistas_por_Espetaculo(void);
-char* tela_Ingressos_por_Usuario(void);
-
-
-void relat_Ingressos_por_Espetaculo(char*);
-void relat_Artistas_por_Espetaculo(char*);
-void relat_Ingressos_por_Usuario(char*);
-
-v
-char* getNomeUsuario(char*);
